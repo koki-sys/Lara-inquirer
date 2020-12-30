b196d47d165bc79c94de44976906b59ddcea3a63
koki-sys <soraisu7@gmail.com>
2020-12-30 18:35:54 +0900

    feat: category show

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Area;
 use App\Models\Category;
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -14,5 +15,12 @@ class CategoryController extends Controller
         $areas = Area::all();
         $data = ['categories' => $categories, 'areas' => $areas];
         return view('category.index', $data);
+    }
+
+    public function show($id)
+    {
+        $areabook = Book::where('category_id', $id)->get();
+        $category = Category::find($id);
+        return view('category.show', ['areabook' => $areabook, 'category' => $category]);
     }
 }
