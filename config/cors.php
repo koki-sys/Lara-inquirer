4368d8881ee3c47d39ab7408bf6609505e556559
koki-sys <soraisu7@gmail.com>
2021-04-11 22:30:28 +0900

    fix: Allowed Origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000', 'https://inquirer-rentalsite.netlify.app/'],
 
     'allowed_origins_patterns' => [],
 
